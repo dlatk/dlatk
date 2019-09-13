0a1a3a66b6e2fd654f115fa3c131579414e3a88a
andy <has@cs.stonybrook.edu>
2019-09-13 17:37:07 -0400

    type error for zscore check

--- dlatk/outcomeAnalyzer.py
+++ dlatk/outcomeAnalyzer.py
@@ -575,9 +575,9 @@ class OutcomeAnalyzer(OutcomeGetter):
         return correls
 
     def correlateWithFeatures(self, featGetter, spearman = False, p_correction_method = 'BH', interaction = None,
-                              blacklist=None, whitelist=None, includeFreqs = False, outcomeWithOutcome = False, outcomeWithOutcomeOnly = False,
-                              zscoreRegression = True, logisticReg = False, cohensD = False, outputInteraction = False, 
-                              groupsWhere = ''):
+                              blacklist=None, whitelist=None, includeFreqs = False, outcomeWithOutcome = False,
+                              outcomeWithOutcomeOnly = False, zscoreRegression = True, logisticReg = False,
+                              cohensD = False, outputInteraction = False, groupsWhere = ''):
         """Finds the correlations between features and outcomes
 
         Parameters
@@ -643,7 +643,11 @@ class OutcomeAnalyzer(OutcomeGetter):
                                                       for x in dlac.switchColumnsAndRows(X)])
                             y = rankdata(y)
                         if zscoreRegression:
-                            (X, y) = (zscore(X), zscore(y) if not logisticReg else y)
+                            try: 
+                                (X, y) = (zscore(X), zscore(y) if not logisticReg else y)
+                            except TypeError:
+                                dlac.warn("zscore got type error -- you probably are trying to use non-numeric data")
+                                sys.exit(1)
                         results = None
                         try:
                             means = None

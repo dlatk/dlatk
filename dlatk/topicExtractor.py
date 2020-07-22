b4d5b53e366442d61ff192c4901308ccfdf6beaa
Garrick Sherman <garricks@sas.upenn.edu>
2020-07-22 15:40:58 -0400

    Squashed commit of the following:
    commit 2683df751a5a4ceee33ecf3a37e73391f3f6df4e
    Merge: e7ec347 329fc4c
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 15:21:31 2020 -0400
    
        Merge branch 'pymallet' of github.com:gtsherman/dlatk into pymallet
    
    commit e7ec347e3e71b3880dc261da60e7caa6783a1465
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 15:21:17 2020 -0400
    
        added dlatk-pymallet as dependency
    
    commit 329fc4c62831c44f7c132c6fb4a0bbb0e1038d77
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 15:07:33 2020 -0400
    
        remove gensim check since it is required
    
    commit 97acd3f9217487eabfeea6dd7058d7bb483aecb7
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 15:04:28 2020 -0400
    
        moved gensim into install_requires
    
    commit f347daeee0dde62d2aaa69d3f0e3e6e9dd4b3442
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 15:00:02 2020 -0400
    
        fixing issue from missed merge conflict
    
    commit a6a3190d31eefd86014f7de84d20e705565ce2b5
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 14:52:19 2020 -0400
    
        removing pymallet
    
    commit 54f1265c4a209202d988c697a9f3d8ea964dba74
    Merge: 0ab6de5 c2d2f66
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 14:48:19 2020 -0400
    
        Merge branch 'pymallet' of github.com:gtsherman/dlatk into pymallet
    
    commit 0ab6de517bbb3b8b4d8a4f9b3dd619ff13856925
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Wed Jul 22 14:43:02 2020 -0400
    
        broke pymallet out; added gensim lda option; other lda work
    
    commit c2d2f66a5d1fa3c83079719771d7f189487db5c8
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Mon Jun 8 16:48:07 2020 -0400
    
        added pymallet; run full mallet interface with --estimate_lda_topics
    
    commit 35910d68730c6ef79c896fe6dad8577c7b241b59
    Author: Garrick Sherman <garricks@sas.upenn.edu>
    Date:   Mon Jun 8 16:48:07 2020 -0400
    
        added pymallet; run full mallet interface with --estimate_lda_topics

--- dlatk/topicExtractor.py
+++ dlatk/topicExtractor.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 #########################################
-from numpy import log2, isnan
+import re
+
 import csv
 import os, sys
+
+from dlatk.messageTransformer import MessageTransformer
+
 sys.path.append(os.path.dirname(os.path.realpath(__file__)).replace("/dlatk/LexicaInterface",""))
 
 from dlatk.featureExtractor import FeatureExtractor
 from dlatk import dlaConstants as dlac
 
+from gensim import corpora
+from gensim.models.wrappers import LdaMallet
+
+from numpy import log2, isnan
+from pymallet import defaults
+from pymallet.lda import estimate_topics
+
 from json import loads
 
 class TopicExtractor(FeatureExtractor):
 
-    def __init__(self, corpdb=dlac.DEF_CORPDB, corptable=dlac.DEF_CORPTABLE, correl_field=dlac.DEF_CORREL_FIELD, 
-                 mysql_host = "localhost", message_field=dlac.DEF_MESSAGE_FIELD, messageid_field=dlac.DEF_MESSAGEID_FIELD, 
+    def __init__(self, corpdb=dlac.DEF_CORPDB, corptable=dlac.DEF_CORPTABLE, correl_field=dlac.DEF_CORREL_FIELD,
+                 mysql_host = "localhost", message_field=dlac.DEF_MESSAGE_FIELD, messageid_field=dlac.DEF_MESSAGEID_FIELD,
                  encoding=dlac.DEF_ENCODING, use_unicode=dlac.DEF_UNICODE_SWITCH, ldaMsgTable =dlac.DEF_LDA_MSG_TABLE):
         super(TopicExtractor, self).__init__(corpdb, corptable, correl_field, mysql_host, message_field, messageid_field, encoding, use_unicode)
         self.ldaMsgTable = ldaMsgTable
@@ -150,7 +161,7 @@ class TopicExtractor(FeatureExtractor):
         cfRows = self._executeGetList(usql)#SSCursor woudl be better, but it loses connection
         dlac.warn("finding messages for %d '%s's"%(len(cfRows), self.correl_field))
         self._disableTableKeys(featureTableName)#for faster, when enough space for repair by sorting
-        for cfRow in cfRows: 
+        for cfRow in cfRows:
             cf_id = cfRow[0]
             mids = set() #currently seen message ids
             freqs = dict() #holds frequency of n-grams
@@ -172,7 +183,7 @@ class TopicExtractor(FeatureExtractor):
                         topicId = topic['topic_id']
                         if not topicId in freqs:
                             freqs[topicId] = 1
-                        else: 
+                        else:
                             freqs[topicId] += 1
                     mids.add(message_id)
 
@@ -181,7 +192,7 @@ class TopicExtractor(FeatureExtractor):
             totalInsts = float(totalInsts) #to avoid casting each time below
             rows = [(k, v, valueFunc((v / totalInsts))) for k, v in freqs.items() ] #adds group_norm and applies freq filter
             self._executeWriteMany(wsql, rows)
-        
+
         dlac.warn("Done Reading / Inserting.")
 
         dlac.warn("Adding Keys (if goes to keycache, then decrease MAX_TO_DISABLE_KEYS or run myisamchk -n).")
@@ -190,3 +201,51 @@ class TopicExtractor(FeatureExtractor):
         return featureTableName;
 
 
+class LDAEstimator(object):
+    def __init__(self, feature_getter, num_topics, alpha, beta, iterations, num_stopwords=50, no_stopping=False):
+        self.feature_getter = feature_getter
+        self.num_topics = num_topics
+        self.alpha = alpha
+        self.beta = beta
+        self.iterations = iterations
+        self.num_stopwords = num_stopwords
+        self.no_stopping = no_stopping
+
+        self._stopwords = None
+
+    @property
+    def stopwords(self):
+        if self._stopwords is None:
+            self._stopwords = set()
+            if not self.no_stopping:
+                top_feats = self.feature_getter.getTopFeats(n=self.num_stopwords)
+                for top_feat_row in top_feats:
+                    self._stopwords.add(top_feat_row[0])
+                print('Automatically removed stopwords: {}'.format(str(self._stopwords)))
+        return self._stopwords
+
+    def estimate_topics(self, feature_lines_file, mallet_path=None):
+        if not mallet_path:
+            print('Estimating LDA topics using PyMallet.')
+            estimate_topics(feature_lines_file, num_topics=self.num_topics, alpha=self.alpha, beta=self.beta,
+                            iterations=self.iterations, stoplist=self.stopwords)
+            state_file = defaults.OUTPUT_STATE_FILE
+        else:
+            print('Estimating LDA topics using Mallet.')
+            id2word = corpora.Dictionary(line for line in self._load_corpus(feature_lines_file))
+            mallet = LdaMallet(mallet_path, corpus=self._load_corpus(feature_lines_file, dictionary=id2word),
+                               id2word=id2word, num_topics=self.num_topics, alpha=self.alpha,
+                               iterations=self.iterations)
+            state_file = mallet.fstate()
+        return state_file
+
+    def _load_corpus(self, feature_lines_file, dictionary=None):
+        token_regex = r'(#|@)?(?!(\W)\2+)([a-zA-Z\_\-\'0-9\(-\@]{2,})'
+        with open(feature_lines_file) as f:
+            for line in f:
+                _, _, line = line.split(' ', 2)
+                tokens = [token.group(0) for token in re.finditer(token_regex, line) if token.group(0) not in self.stopwords]
+                if dictionary is None:
+                    yield tokens
+                else:
+                    yield dictionary.doc2bow(tokens)

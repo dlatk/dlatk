e2a7dedcd687f3e09bf6049e0f00d21622d4380a
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2022-03-04 20:34:56 -0500

    added transformer embs file to serve as an API for user embedding generation

--- dlatk/transformer_embs.py
+++ dlatk/transformer_embs.py
@@ -1,16 +1,44 @@
+from typing import Optional, List
+import json
+
 ######################
 
 def prune_transformer_layers(model):
     return
 
-class transformer_embeddings():
+class sentence_tokenizer:
+    """
+        NLTK sentence tokenizer
+    """
+    def __init__(self):
+        try:
+            import nltk.data
+            import sys
+        except ImportError:
+             print("warning: unable to import nltk.tree or nltk.corpus or nltk.data")
+
+        self.sentDetector = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    def __call__(self, messageRows):
+        
+        messages = list(map(lambda x: x[1], messageRows))
+        parses = []
+        for m_id, message in messageRows:
+            parses.append([m_id, json.dumps(self.sentDetector.tokenize(tc.removeNonUTF8(tc.treatNewlines(message.strip()))))])
+        return parses
+        
+
+class transformer_embeddings:
     """
     Class to retrieve transformer embeddings. Supply table name, model name, layer numbers, aggregation methods, [device numbers], [output table name]
     """
-    def __init__(self, table_name: str, model_name_or_path: str, batch_size: int = 32, emb_table_name: str = None, save_path: str = None):
+    #modelName, tokenizerName, modelClass=None, batchSize=dlac.GPU_BATCH_SIZE, aggregations = ['mean'], layersToKeep = [8,9,10,11], maxTokensPerSeg=255, noContext=True, layerAggregations = ['concatenate'], wordAggregations = ['mean'], keepMsgFeats = False, customTableName = None, valueFunc = lambda d: d
+    
+    def __init__(self, tableName:str, modelName:str, tokenizerName:str=None, layersToKeep:List=[-1, -2, -3, -4], aggregations:List=['mean'], layerAggregations:List=['mean'], wordAggregations:List=['mean'], maxTokensPerSeg=None, batchSize:int=dlac.GPU_BATCH_SIZE, noContext=True, customTableName:str=None, savePath:str=None):
+        
         self.groups_processed = 0
         self.groups = [] #run get_groups()
-        self.batch_size = batch_size
+        self.batchSize = batchSize   
         
     def load_text(self, ):
         """

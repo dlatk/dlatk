58d4ccd804d794cf97be46fb39c84db32060e340
shshnk94 <shshnk94@gmail.com>
2023-04-05 23:44:31 +0000

    updated colabify.sh to exclude Python install and include MySQL 8.0

--- dlatk/tools/colabify.sh
+++ dlatk/tools/colabify.sh
@@ -3,26 +3,13 @@
 # DLATK post-installation instructions.
 # This script is executed when dlatkInterface.py is called with --colabify flag.
 
-#Install Python 3.6 if not installed already
-if ! python --version | grep -q "3.6" 
+if ! mysql --version
 then 
-  
-  echo "Default Python not 3.6."
-  if ! [ -f /usr/bin/python3.6 ]; then
-    apt-get install python3.6 -y
-  else
-    echo "Python 3.6 exists in /usr/bin. Updating alternatives." 
-  fi
-
-  update-alternatives --install /usr/local/bin/python python /usr/bin/python3.6 1 --force  
-  python --version
-fi
-
-#Install MySQL 5.7 if not installed already
-if ! mysql --version | grep -q "5.7" 
-then 
-  echo "MySQL 5.7 not found. Installing it now..."
-  apt-get install mysql-server-5.7 mysql-client-5.7 -y
+  echo "MySQL not found. Installing it now..."
+  apt-get install mysql-server mysql-client -y
+  echo '[mysqld]
+  skip_log_bin=1
+  ' >> /etc/mysql/mysql.conf.d/mysqld.cnf
   service mysql start
 fi
 
@@ -30,8 +17,12 @@ DLATK_PATH=$1 #Path to DLATK passed from dlatkInterface.py
 mysql < ${DLATK_PATH}/data/dla_tutorial.sql
 mysql < ${DLATK_PATH}/data/dlatk_lexica.sql
 
-#Install mallet 2.0.8
+# Install mallet 2.0.8
 wget -q -O mallet.tar.gz http://mallet.cs.umass.edu/dist/mallet-2.0.8.tar.gz
 mkdir -p /opt/mallet
 tar -xf mallet.tar.gz -C /opt/mallet --strip-components=1
 rm mallet.tar.gz
+
+# Install Python dependencies
+pip install -r /content/dlatk/install/requirements.txt
+pip install rpy2==3.5.1

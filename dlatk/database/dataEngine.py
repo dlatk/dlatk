ce4eb75c22b55d79ab6834ef882ac53de9c42e94
shshnk94 <shshnk94@gmail.com>
2023-12-07 18:31:42 +0000

    fixed minimum sample size issue in column_description

--- dlatk/database/dataEngine.py
+++ dlatk/database/dataEngine.py
@@ -292,7 +292,7 @@ class DataEngine(object):
 		#read a random sample of size 100 from the CSV.	
 		sample_size = 1000
 		num_lines = int(check_output(["wc", "-l", csv_file]).split()[0])
-		row_idx = sorted(random.sample(range(1, num_lines), sample_size))
+		row_idx = sorted(random.sample(range(1, num_lines), min(num_lines, sample_size)))
 		with open(csv_file, 'r') as f:
 			reader = csv.reader(f, delimiter=',')
 			header = next(reader)

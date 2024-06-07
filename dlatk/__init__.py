7a1105d75ed39d6a17a5e5f06ba927e0b81fe9ad
Salvatore Giorgi <sal.giorgi@gmail.com>
2024-06-06 22:05:57 -0400

    updated __init__.py with version number

--- dlatk/__init__.py
+++ dlatk/__init__.py
@@ -1,7 +1,7 @@
 __authors__ = "H. Andrew Schwartz, Salvatore Giorgi, Maarten Sap, Patrick Crutchley, Lukasz Dziurzynski and Megha Agrawal"
 __copyright__ = "Copyright 2020"
 __credits__ = []
-__license__ = "Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License: http://creativecommons.org/licenses/by-nc-sa/3.0/"
+__license__ = "Cr3a1ive Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License: http://creativecommons.org/licenses/by-nc-sa/3.0/"
 __version__ = "1.1.4" 
 __maintainer__ = "Salvatore Giorgi, Andy Schwartz"
 __email__ = "sgiorgi@sas.upenn.edu, has@cs.stonybrook.edu"

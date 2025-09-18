1a8165d4d0787db3ae0322fcba9b158a5d1ed8f4
Tony Bui <longphibui1993@gmail.com>
2025-09-18 17:31:04 -0400

    update version

--- setup.py
+++ setup.py
@@ -154,8 +154,8 @@ INSTALL_REQUIRES = [
     "python-dateutil>=2.5.0,<3.0",
 
     # NumPy
-    'numpy>=1.20,<2.0; python_version<="3.12"',
-    'numpy>=2.0; python_version>"3.12"',
+    'numpy>=1.20,<2.0; python_version<"3.12"',
+    'numpy>=2.0; python_version>="3.12"',
 
     # pandas
     'pandas>=1.2,<2.0; python_version<"3.12"',
